e5d0d351f7494d22be8a12222c4f739b36949f12
zx <3313147896@qq.com>
2021-01-26 14:37:19 +0800

    首页导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,27 @@
 <!--  -->
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
     <h2>首页</h2>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  data () {
-    return {
-    };
+  data() {
+    return {};
   },
-}
-
+  components: {
+    NavBar
+  }
+};
 </script>
 <style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
